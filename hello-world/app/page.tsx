c14c6412c6a432c30831d015f957f859d777fc82
Rafael Santana <rafael.s.santana43@outlook.com>
2025-10-23 18:48:14 -0300

    Delete hello-world directory
    Acessar o branch atividade1

--- hello-world/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 via-white to-purple-100 text-gray-800">
-      <h1 className="text-5xl font-bold mb-4">Hello World! 😀</h1>
-    </div>
-  );
-}

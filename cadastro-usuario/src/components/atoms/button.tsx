ddeff9155447a1eb055186ed1970c1a78239c7bf
Lávio Vale <laviovale@gmail.com>
2025-10-23 16:54:59 -0300

    feat: tornando o botão clicável no hover

--- cadastro-usuario/src/components/atoms/button.tsx
+++ cadastro-usuario/src/components/atoms/button.tsx
@@ -8,7 +8,7 @@ export function Button({ children, ...props }: ButtonProps) {
   return (
     <button
       className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm 
-                 text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 
+                 text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 cursor-pointer
                  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       {...props}
     >
